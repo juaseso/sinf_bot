340cfd419810b22f92e42eb5aae37a872d647d4d
juaseso <trabajos.juanma@gmail.com>
2018-04-12 23:18:38 +0200

    Implemented a lot of functions

--- store.py
+++ store.py
@@ -2,6 +2,7 @@ from conn_conf import client
 
 db = client.proyecto_sinf
 cpaises = db.paises
+cnoticias = db.noticias
 
 def insert_pais(update):
 	cpaises.insert({"usuario_id":update.message.chat.id,
@@ -9,7 +10,7 @@ def insert_pais(update):
 
 
 def update_user(update):
-    cpaises.update({"usuario_id": update.message.chat.id}, {"$push": {"cpaisescula":update.message.text}})
+    cpaises.update({"usuario_id": update.message.chat.id}, {"$push": {"cplicula":update.message.text}})
 
 
 def pais_exists(update):
@@ -21,3 +22,37 @@ def pais_exists(update):
     except Exception as e:
         return bol
 
+		
+def getNoticia(): 
+	try: 
+		noticia = cnoticias.find({'n_analisis':{'$lt':5} }).sort('n_analisis',1).limit(1)[0]
+		return noticia
+	except Exception as e:
+	return None
+	
+def getTitular(noticia):
+	return noticia['titular']
+	
+def getCuerpo(noticia):
+	return noticia['cuerpo']
+	
+def marcarNoticia(noticia): 
+	cnoticias.update(
+		{
+			'item_id' : noticia['item_id'] 
+		}, 
+		{
+			'$set' : {'invalida':true}
+		}
+	)
+	
+def clasificaPais(update, noticia): 
+	nombre_pais = update.message.text
+	cnoticias.update(
+		{
+			'item_id' : noticia['item_id'] 
+		}, 
+		{
+			'$push': {'posibles_paises': nombre_pais}	
+		}
+	)
