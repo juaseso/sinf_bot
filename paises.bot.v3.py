eae9e516bce6f9bc99c896172041db5f6b47d431
juaseso <trabajos.juanma@gmail.com>
2018-04-13 21:54:13 +0200

    minor changes

--- paises.bot.v3.py
+++ paises.bot.v3.py
@@ -1,47 +1,42 @@
 import logging
-import store
+from store import getNoticia, getTitular, getCuerpo, marcarNoticia, clasificaPais, paisExists
 from telegramkey import Token
 from telegram.ext import Updater, CommandHandler, ConversationHandler, Filters, MessageHandler, Handler
 
 noticia = {}
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-
 CLASIFICAR_INI, CLASIFICAR_TITULO_SUFICIENTE, CLASIFICAR_CUERPO_SUFICIENTE, CLASIFICAR_PAIS = range(4)
 
-
 def inicio(bot, update):
-	bot.sendMessage(chat_id=update.message.chat.id, text="hola {} ¿Quieres clasificar una noticia?".format(update.message.chat.first_name))
+	bot.sendMessage(chat_id=update.message.chat.id, text="Hola {}, ¿Quieres clasificar una noticia?".format(update.message.chat.first_name))
 	return CLASIFICAR_INI
 
 def clasificar_ini(bot, update):
+	global noticia 
 	noticia = getNoticia()
 	return decisor(1, bot, update)
-	
+
 def clasificar_titulo_suficiente(bot, update): 
 	return decisor(2, bot, update)
-	
+
 def clasificar_cuerpo_suficiente(bot, update): 
 	return decisor(4, bot, update)
-	
+
 def clasificar_pais(bot, update):
-	if pais_exists(update): 
+	if paisExists(update): 
 		clasificaPais(update, noticia)
 		bot.sendMessage(chat_id=update.message.chat.id, text='Noticia clasificada correctamente, gracias por participar.')
 		return decisor(3,bot,update)
 	else: 
-		bot.sendMessage(chat_id=update.message.chat.id, text='El país introducido no existe, prueba de nuevo sin usar acentos o caracteres no alfanuméricos')
+		bot.sendMessage(chat_id=update.message.chat.id, text='El país introducido no existe, prueba de nuevo sin usar acentos o caracteres no alfabéticos')
 		return CLASIFICAR_PAIS
-		
 
-	
-	
 def decisor(d, bot, update): 
 
 	# Decision 1: Pregunta si quiere clasificar una noticia
 	if d == 1: 
 		if update.message.text.lower() == 'si' or update.message.text.lower() == 'sí':
-			bot.sendMessage(chat_id=update.message.chat.id, text=getTitular(noticia))
+			bot.sendMessage(chat_id=update.message.chat.id, text="Titular: "+getTitular(noticia))
 			bot.sendMessage(chat_id=update.message.chat.id, text="¿Es posible decir a qué país pertenece con esta información?")
 			return CLASIFICAR_TITULO_SUFICIENTE
 		else:
@@ -49,10 +44,10 @@ def decisor(d, bot, update):
 			return ConversationHandler.END
 
 			
-	# Decision 2: Pregunta si la info es suficiente para dar una respuesta
+	# Decision 2: Pregunta si con el titular es suficiente para dar una respuesta
 	elif d == 2:
 		if update.message.text.lower() == 'si' or update.message.text.lower() == 'sí':
-			bot.sendMessage(chat_id=update.message.chat.id, text="Introduce el nombre del país, por favor")
+			bot.sendMessage(chat_id=update.message.chat.id, text="1-Introduce el nombre del país, por favor")
 			return CLASIFICAR_PAIS
 		else:
 			bot.sendMessage(chat_id=update.message.chat.id, text=getCuerpo(noticia))
@@ -66,7 +61,7 @@ def decisor(d, bot, update):
 		return CLASIFICAR_INI
 		
 		
-	# Decision 4: Pregunta si la info es suficiente para dar una respuesta
+	# Decision 4: Pregunta si el cuerpo es suficiente para dar una respuesta
 	elif d == 4: 
 		if update.message.text.lower() == 'si' or update.message.text.lower() == 'sí':
 			bot.sendMessage(chat_id=update.message.chat.id, text="Introduce el nombre del país, por favor")
@@ -76,14 +71,10 @@ def decisor(d, bot, update):
 			marcarNoticia(noticia)
 			bot.sendMessage(chat_id=update.message.chat.id, text='¿Quieres seguir clasificando noticias?')
 			return CLASIFICAR_INI
-		
-	
-	
-	
+
 def cancel(bot, update):
-	bot.sendMessage(chat_id=update.message.chat.id, text="Prueba a entrar más tarde")
+	bot.sendMessage(chat_id=update.message.chat.id, text="Conversación reiniciada")
 	return ConversationHandler.END
-	
 
 def main():
     update = Updater(str(Token))
@@ -99,7 +90,6 @@ def main():
     update.dispatcher.add_handler(conversacion)
     update.start_polling()
     update.idle()
-
 
 if __name__ == '__main__':
     main()
--- store.py
+++ store.py
@@ -4,16 +4,7 @@ db = client.proyecto_sinf
 cpaises = db.paises
 cnoticias = db.noticias
 
-def insert_pais(update):
-	cpaises.insert({"usuario_id":update.message.chat.id,
-                 "edad":update.message.text})
-
-
-def update_user(update):
-    cpaises.update({"usuario_id": update.message.chat.id}, {"$push": {"cplicula":update.message.text}})
-
-
-def pais_exists(update):
+def paisExists(update):
     try:
         bol = False
         if cpaises.find({"nombrePais": update.message.text}).count() > 0:
@@ -22,37 +13,31 @@ def pais_exists(update):
     except Exception as e:
         return bol
 
-		
 def getNoticia(): 
 	try: 
 		noticia = cnoticias.find({'n_analisis':{'$lt':5} }).sort('n_analisis',1).limit(1)[0]
 		return noticia
 	except Exception as e:
 		return None
-	
+		
+		
+def getNoticiaFalsa(): 
+	try: 
+		noticia = cnoticias.find({'n_analisis':{'$eq':3} }).sort('n_analisis',1).limit(1)[0]
+		return noticia
+	except Exception as e:
+		return None
+
 def getTitular(noticia):
-	return noticia['titular']
-	
+	titular = noticia['titular']
+	return titular
+
 def getCuerpo(noticia):
 	return noticia['cuerpo']
-	
+
 def marcarNoticia(noticia): 
-	cnoticias.update(
-		{
-			'item_id' : noticia['item_id'] 
-		}, 
-		{
-			'$set' : {'invalida':true}
-		}
-	)
-	
+	cnoticias.update({'item_id' : noticia['item_id']}, {'$set' : {'invalida': True }})
+
 def clasificaPais(update, noticia): 
 	nombre_pais = update.message.text
-	cnoticias.update(
-		{
-			'item_id' : noticia['item_id'] 
-		}, 
-		{
-			'$push': {'posibles_paises': nombre_pais}	
-		}
-	)
+	cnoticias.update({'item_id' : noticia['item_id']}, {'$push': {'posibles_paises': nombre_pais}})
